06636acf415feb76c9335ce96fec353c79f5fd5e
Yar-nikita <74370771+Yar-nikita@users.noreply.github.com>
2021-05-26 14:54:03 +0500

    Add files via upload

---
+++ config.js
@@ -0,0 +1,26 @@
+module.exports = {
+   HOST: 'us-cdbr-east-03.cleardb.com',
+	DBUSER: 'b3df0bd7b2f55d',
+	DBPASSWORD: '45b5db32',
+	DBNAME: 'service_center',
+   jwt: 'jwt-key' //Your jwt key for token generate
+}
+
+
+
+// module.exports = {
+//    HOST: '127.0.0.1',
+// 	DBUSER: 'root',
+// 	DBPASSWORD: 'root',
+// 	DBNAME: 'service_center',
+//    jwt: 'jwt-key' //Your jwt key for token generate
+// }
+// mysql://
+// b3df0bd7b2f55d  USER NAME 
+// :
+// 45b5db32 PASSWORD
+// @
+// us-cdbr-east-03.cleardb.com HOST 
+// /heroku_77aa8160070c1ed?reconnect=true
+
+
